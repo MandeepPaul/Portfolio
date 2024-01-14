c9f8ee173b4e782888c5d02577f39a48ab16864d
MandeepPaul <113959405+MandeepPaul@users.noreply.github.com>
2024-01-14 13:58:25 -0500

    Added skills and seperated source file

--- components/Main/SubSection/Skills.js
+++ components/Main/SubSection/Skills.js
@@ -1,15 +1,31 @@
 import SkillCard from "@/components/UI/Card/SkillCard";
-
+import { Skills as SkillsArray, AdditionalSkills } from "@/utils/Skills";
 const Skills = () => {
   return (
-    <section className="mx-4 p-1 bg-neutral-900 bg-opacity-70 rounded-lg space-y-4">
-      <h2 className="text-xl md:text-3xl lg:text-4xl">
-        My Choice <span className="gradient-text-teal-sky">Tech Stack</span>
-      </h2>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2">
-        <SkillCard />
-      </div>
-    </section>
+    <>
+      <section className="mx-4 p-1 bg-neutral-900 bg-opacity-70 rounded-lg space-y-4">
+        <h2 className="text-xl md:text-3xl lg:text-4xl">
+          My Choice <span className="gradient-text-teal-sky">Tech Stack</span>
+        </h2>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2">
+          {SkillsArray.map((eachSkill, index) => (
+            <SkillCard key={index} {...eachSkill} />
+          ))}
+        </div>
+      </section>
+
+      <section className="mx-4 p-1 bg-neutral-900 bg-opacity-70 rounded-lg space-y-4">
+        <h2 className="text-xl md:text-3xl lg:text-4xl">
+          Some other <span className="gradient-text-teal-sky">Tech Stack</span>{" "}
+          that <span className="gradient-text-teal-sky">I love</span>
+        </h2>
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 gap-2">
+          {AdditionalSkills.map((eachSkill, index) => (
+            <SkillCard key={index} {...eachSkill} />
+          ))}
+        </div>
+      </section>
+    </>
   );
 };
 
